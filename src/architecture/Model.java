91d57aa44967057be3fe21ac4dde4f95b719f280
a5bbbbb <suprunoviktor@gmail.com>
2024-11-05 13:41:00 +0500

    Commit 0.1: Model and CartSingleton added

---
+++ src/architecture/Model.java
@@ -0,0 +1,13 @@
+package architecture;
+
+public class Model {
+    private double totalPrice;
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
